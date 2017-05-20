649bef9f39c8f225dfb4a9be0122555359d1651c
Nathan Struhs <nathanstruhs@gmail.com>
2017-05-20 13:46:00 -0700

    Configure video capture

---
+++ Ecotone/PhotoViewController.swift
@@ -0,0 +1,44 @@
+//
+//  PhotoViewController.swift
+//  Ecotone
+//
+//  Created by Nathan Struhs on 5/20/17.
+//  Copyright © 2017 OSU Cascades. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+    
+    var takenPhoto:UIImage?
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let availableImage = takenPhoto {
+            imageView.image = availableImage
+        }
+    }
+
+    @IBAction func goBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Ecotone/PlantIdentificationViewController.swift
+++ Ecotone/PlantIdentificationViewController.swift
@@ -6,20 +6,118 @@
 //  Copyright © 2017 OSU Cascades. All rights reserved.
 //
 
-import UIKit
+// source: https://www.youtube.com/watch?v=Zv4cJf5qdu0
 
-class PlantIdentificationViewController: UIViewController {
+import UIKit
+import AVFoundation
+
+class PlantIdentificationViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    let captureSession = AVCaptureSession()
+    var previewLayer:CALayer!
+    
+    var captureDevice : AVCaptureDevice!
+    
+    var takePhoto = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // make the camera video appear
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        prepareCamera()
+    }
+    
+    func prepareCamera() {
+        captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+        
+        if let availableDevices = AVCaptureDeviceDiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaTypeVideo, position: .back).devices {
+            captureDevice = availableDevices.first
+            beginSession()
+        }
+    }
+    
+    func beginSession() {
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(captureDeviceInput)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        if let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession) {
+            self.previewLayer = previewLayer
+            self.view.layer.addSublayer(self.previewLayer)
+            self.previewLayer.frame = self.view.layer.frame
+            captureSession.startRunning()
+            
+            let dataOutput = AVCaptureVideoDataOutput()
+            dataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString):NSNumber(value:kCVPixelFormatType_32BGRA)]
+            
+            dataOutput.alwaysDiscardsLateVideoFrames = true
+            
+            if captureSession.canAddOutput(dataOutput) {
+                captureSession.addOutput(dataOutput)
+            }
+            
+            captureSession.commitConfiguration()
+            
+            let queue = DispatchQueue(label: "com.ecotone.captureQueue")
+            dataOutput.setSampleBufferDelegate(self, queue: queue)
+            
+        }
+    }
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        takePhoto = true
+    }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!) {
+        if takePhoto {
+            takePhoto = false
+            if let image = self.getImageFromSampleBuffer(buffer: sampleBuffer) {
+                DispatchQueue.main.async {
+                    let photoVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PhotoVC") as! PhotoViewController
+                    
+                    photoVC.takenPhoto = image
+                    
+                    DispatchQueue.main.async {
+                        self.present(photoVC, animated: true, completion: {
+                            self.stopCaptureSession()
+                        })
+                    }
+                }
+            }
+        }
+    }
+    
+    func getImageFromSampleBuffer (buffer:CMSampleBuffer) -> UIImage? {
+        if let pixelBuffer = CMSampleBufferGetImageBuffer(buffer) {
+            let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+            let context = CIContext()
+            
+            let imageRect = CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(pixelBuffer), height: CVPixelBufferGetHeight(pixelBuffer))
+            
+            if let image = context.createCGImage(ciImage, from: imageRect) {
+                return UIImage(cgImage: image, scale: UIScreen.main.scale, orientation: .right)
+            }
+        }
+        return nil
+    }
+    
+    func stopCaptureSession() {
+        self.captureSession.stopRunning()
+        
+        if let inputs = captureSession.inputs as? [AVCaptureDeviceInput] {
+            for input in inputs {
+                self.captureSession.removeInput(input)
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
