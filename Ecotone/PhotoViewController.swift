034feb0575f16833277a0a93564a731b145c25c6
Yong Bakos <ybakos@humanoriented.com>
2017-05-21 16:23:52 -0700

    Rename UIImage member, remove explicit goBack and binding.

--- Ecotone/PhotoViewController.swift
+++ Ecotone/PhotoViewController.swift
@@ -6,35 +6,19 @@ import UIKit
 
 class PhotoViewController: UIViewController {
 
-    var takenPhoto:UIImage?
-
     @IBOutlet weak var imageView: UIImageView!
 
+    var photo: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let availableImage = takenPhoto {
-            imageView.image = availableImage
+        if let photo = self.photo {
+            imageView.image = photo
         }
-    }
-
-    @IBAction func goBack(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
