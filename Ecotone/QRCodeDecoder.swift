ed9bfdfb0ba33b2f1b631eddc9ee0da7407f1bf8
Yong Bakos <ybakos@humanoriented.com>
2017-05-28 00:23:08 -0700

    Extract assignment of supportedCodeTypes to QRCodeDecoder. Explicitly group the adding of the output and the assignment of supported code types into a function, and document the coupling of this behavior. References #2.

--- Ecotone/QRCodeDecoder.swift
+++ Ecotone/QRCodeDecoder.swift
@@ -21,7 +21,20 @@ class QRCodeDecoder {
             input = nil
             print(error)
         }
+        addOutputAndSetMetadataObjectTypes()
+    }
+
+    // Add the capture output to the capture session, and set the capture output's
+    // metadata object types. These operations must be done together, in this order.
+    func addOutputAndSetMetadataObjectTypes() {
         captureSession.addOutput(captureMetadataOutput)
+        // Devices that do not support AVMetadataObjectTypeQRCode, such as the simulator,
+        // will crash when assigning unsupported code types. Check that the AVMetadataObjectTypeQRCode
+        // type is supported before assigning.
+        if let availableMetadataObjectTypes = captureMetadataOutput.availableMetadataObjectTypes as? [String],
+            availableMetadataObjectTypes.contains(AVMetadataObjectTypeQRCode) {
+            captureMetadataOutput.metadataObjectTypes = supportedCodeTypes
+        }
     }
     
     func setMetadataObjectsDelegate(_ delegate: AVCaptureMetadataOutputObjectsDelegate, queue: DispatchQueue) {
