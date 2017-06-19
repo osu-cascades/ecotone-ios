bfdd0e6434a02ca7e7bf24345c5f37f6c589108e
Yong Bakos <ybakos@humanoriented.com>
2017-06-18 23:52:00 -0700

    Remove green outline box during QRCode detection.

--- Ecotone/QRCodeCaptureViewController.swift
+++ Ecotone/QRCodeCaptureViewController.swift
@@ -9,14 +9,12 @@ import AVFoundation
 class QRCodeCaptureViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
 
     let qrCodeDecoder = QRCodeDecoder()
-    let qrCodeFrameView = UIView()
     var videoPreviewLayer: AVCaptureVideoPreviewLayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         qrCodeDecoder.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
         initializeVideoPreviewLayer()
-        initializeQRCodeFrame()
     }
 
     func initializeVideoPreviewLayer() {
@@ -26,13 +24,6 @@ class QRCodeCaptureViewController: UIViewController, AVCaptureMetadataOutputObje
         view.layer.addSublayer(videoPreviewLayer!)
     }
 
-    func initializeQRCodeFrame() {
-        qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
-        qrCodeFrameView.layer.borderWidth = 2
-        view.addSubview(qrCodeFrameView)
-        view.bringSubview(toFront: qrCodeFrameView)
-    }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         UIApplication.shared.statusBarView?.backgroundColor = UIColor.clear
@@ -43,18 +34,10 @@ class QRCodeCaptureViewController: UIViewController, AVCaptureMetadataOutputObje
     func captureOutput(_ captureOutput: AVCaptureOutput!,
                        didOutputMetadataObjects metadataObjects: [Any]!,
                        from connection: AVCaptureConnection!) {
-        if metadataObjects == nil || metadataObjects.isEmpty {
-            qrCodeFrameView.frame = CGRect.zero
-            return
-        }
         if let metadataObj = metadataObjects[0] as? AVMetadataMachineReadableCodeObject,
-            qrCodeDecoder.supportedCodeTypes.contains(metadataObj.type) {
-            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
-            qrCodeFrameView.frame = barCodeObject!.bounds
-            if let qrCodeDecodedString = metadataObj.stringValue {
-                qrCodeFrameView.frame = CGRect.zero
+            qrCodeDecoder.supportedCodeTypes.contains(metadataObj.type),
+            let qrCodeDecodedString = metadataObj.stringValue {
                 showURLInWebApplicationTab(path: qrCodeDecodedString)
-            }
         }
     }
     
